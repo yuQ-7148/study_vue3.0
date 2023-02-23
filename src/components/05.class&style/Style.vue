87ae007aa83de9ff0464a9c0c36a75b88bfb8b0a
yuQ-7148 <1159915745@qq.com>
2023-02-23 17:10:41 +0800

    动态绑定style

--- src/components/05.class&style/Style.vue
+++ src/components/05.class&style/Style.vue
@@ -5,6 +5,12 @@
         <h3 class="thin" :class="classObj">MyStyle 组件</h3>
         <button @click="classObj.italic = !classObj.italic">Toggle Italic</button>
         <button @click="classObj.delete = !classObj.delete">Toggle Delete</button>
+
+        <hr>
+
+        <div :style="{ color: active, fontSize: fsize + 'px', 'background-color': bgcolor }">MyStyle 组件</div>
+        <button @click="fsize += 1">字号 +1</button>
+        <button @click="fsize -= 1">字号 -1</button>
     </div>
 </template>
 
@@ -18,7 +24,10 @@ export default {
             classObj: {
                 italic: false,
                 delete: false,
-            }
+            },
+            active: 'red',
+            fsize: 40,
+            bgcolor: 'pink'
         }
     }
 }
