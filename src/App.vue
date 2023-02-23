aeed19632f16f1cfd867644594807db00ef53370
yuQ-7148 <1159915745@qq.com>
2023-02-23 09:41:47 +0800

    vue组件结构

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <div>
+    <h3>App.vue 根组件</h3>
+    <h3>{{ username }}</h3>
+  </div>
+
+
+  <hr>
+
+  <h3>{{ count }}</h3>
+  <button @click="addCount()">+1</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'MyApp',
+  data() {
+    return {
+      username: 'zs',
+      count: 0
+    }
+  },
+  methods: {
+    addCount() {
+      this.count += 1
+    }
   }
 }
 </script>
+
+<style lang="less">
+h3 {
+  color: cyan;
+}
+
+div {
+  background-color: cyan;
+  h3 {
+    background-color: red;
+  }
+}
+</style>
