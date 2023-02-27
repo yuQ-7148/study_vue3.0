575cbdd998ed69c56e7accc64eeeefe7628f606b
yuQ-7148 <1159915745@qq.com>
2023-02-27 17:31:43 +0800

    自定义指令

---
+++ src/components/18.directive/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+       <my-home></my-home>
+    </div>
+</template>
+
+<script>
+import MyHome from './Home.vue'
+export default {
+    name: 'MyApp',
+    components: {
+        MyHome
+    }
+}
+</script>
+
+<style scoped></style>
---
+++ src/components/18.directive/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+       <h3>MyHome</h3>
+       <input type="text" v-focus>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyHome',
+    directives: {
+        focus: {
+            mounted(el) {
+                el.focus()
+            }
+        }
+    }
+}
+</script>
+
+<style scoped></style>
