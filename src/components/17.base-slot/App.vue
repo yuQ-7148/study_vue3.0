261aaba35759ab58e095c9516959bb9994a329a9
yuQ-7148 <1159915745@qq.com>
2023-02-27 17:24:12 +0800

    作用域插槽

--- src/components/17.base-slot/App.vue
+++ src/components/17.base-slot/App.vue
@@ -1,18 +1,8 @@
 <template>
     <div>
         <my-com>
-            <template #header>
-                <p>header</p>
-            </template>
-
-            <template #default>
-                <p>111</p>
-                <p>222</p>
-                <p>333</p>
-            </template>
-
-            <template #footer>
-                <p>footer</p>
+            <template #default="{info}">
+                {{ info }}
             </template>
         </my-com>
     </div>
--- src/components/17.base-slot/MyCom.vue
+++ src/components/17.base-slot/MyCom.vue
@@ -5,12 +5,18 @@
         <slot></slot>
         <hr>
         <slot name="footer"></slot>
+        <slot :info="count"></slot>
     </div>
 </template>
 
 <script>
 export default {
     name: 'MyCom',
+    data() {
+        return {
+            count: 0
+        }
+    }
 }
 </script>
 
