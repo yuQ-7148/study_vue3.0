aef87c8f4279a7a7b84d0fa6b316e4247706d404
yuQ-7148 <1159915745@qq.com>
2023-02-27 11:20:33 +0800

    组件的生命周期函数

---
+++ src/components/10.life-cycle/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <life-cycle v-if="flag"></life-cycle>
+        <button @click="flag = !flag">flag</button>
+    </div>
+</template>
+
+<script>
+import LifeCycle from './LifeCycle.vue'
+export default {
+    name: 'MyApp',
+    data() {
+        return {
+            flag: true
+        }
+    },
+    components: {
+        LifeCycle
+    }
+}
+</script>
+
+<style scoped></style>
---
+++ src/components/10.life-cycle/LifeCycle.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+       <h3>{{ count }}</h3>
+       <button @click="count++">+1</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LifeCycle',
+    data() {
+        return {
+            count: 0
+        }
+    },
+    created() {
+        console.log('created');
+    },
+    mounted() {
+        console.log('mounted');
+    },
+    unmounted() {
+        console.log('unmounted');
+    },
+    updated() {
+        console.log('updated');
+    }
+}
+</script>
+
+<style scoped></style>
